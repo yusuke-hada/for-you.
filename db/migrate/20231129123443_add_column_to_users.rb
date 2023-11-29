b4e9a96ba443d10935bafa425e145a35b50a112f
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-11-29 21:47:04 +0900

    User_model

---
+++ db/migrate/20231129123443_add_column_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :role, :integer, default: 0,null: false
+    add_column :users, :age, :integer, null: false
+    add_column :users, :gender, :integer, null: false
+    add_column :users, :business, :string
+    add_column :users, :hobby, :string
+  end
+end
