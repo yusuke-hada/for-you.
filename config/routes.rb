5636a5372ea2bc461f39833f457fe396566d1ad8
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-01 20:01:33 +0900

    create_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/top'
   root to:'pages#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:new, :create, :edit ,:update]
 end
