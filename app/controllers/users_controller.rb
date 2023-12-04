5cd36267d118ff176a1928fdf7daabe386e97aee
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-04 22:18:12 +0900

    ADD:step2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,43 @@
 class UsersController < ApplicationController
-  def new
+  def step1
     @user = User.new
   end
 
-  def create; end
+  def step2
+    session[:name] = user_params[:name]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: session[:name],
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      age: user_params[:age],
+      gender: user_params[:gender],
+      business: user_params[:business],
+      hobby: user_params[:hobby],
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to new_user_path
+    else
+      render '/users/step1'
+    end
+  end
 
   def edit; end
 
   def update; end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :age, :gender, :business, :hobby)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#top'
-  resources :users, only: %i[new create edit update]
+  resources :users, only: %i[new create edit update] do
+    collection do
+      get 'step1'
+      get 'step2'
+    end
+  end
 end
