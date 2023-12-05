45f122d0652ed9d37638497632f3b531b0be574f
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-05 11:36:04 +0900

    ADD:enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :name, length: { maximum: 252 }, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password]|| changes[:reset_password_token] }
+  validates :reset_password_token, uniqueness: true, allow_nil: true
+  validates :age, presence: true
+  validates :gender, presence: true
+  enum gender: { man: 0, woman: 1, other: 2}
+  enum role: { general: 0, admin: 1}
 end
