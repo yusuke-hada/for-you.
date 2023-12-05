45ce575e1b635e2dbbbc015b4cf9937587d87522
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-05 15:24:57 +0900

    ADD:controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'wish_lists/index'
+  get 'wish_lists/new'
+  get 'wish_lists/create'
+  get 'wish_lists/edit'
+  get 'wish_lists/update'
+  get 'wish_lists/destroy'
   root to: 'pages#top'
   resources :users, only: %i[new create edit update] do
     collection do
---
+++ test/controllers/wish_lists_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class WishListsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get wish_lists_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get wish_lists_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get wish_lists_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get wish_lists_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get wish_lists_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get wish_lists_destroy_url
+    assert_response :success
+  end
+end
