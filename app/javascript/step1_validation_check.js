a1f5a3c500a81d23c160ad8f0204ef4375ae7ec1
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-08 01:07:38 +0900

    ADD:email_validation

--- app/javascript/step1_validation_check.js
+++ app/javascript/step1_validation_check.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('registration_form').addEventListener('submit', function(event) {
       let isValid = true;
       let name = document.getElementById('name').value;
+      let email = document.getElementById('email').value;
+      const pattern = /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
 
-      // nameのバリデーション
       if (name.length === 0) {
         document.getElementById('nameError').textContent = '名前を入力してください。';
         document.getElementById('nameError').style.display = 'block';
@@ -15,6 +16,20 @@ document.addEventListener('DOMContentLoaded', function() {
       } else {
         document.getElementById('nameError').style.display = 'none';
       }
+
+      if (email.length === 0){
+        document.getElementById('emailError').textContent = 'メールアドレスを入力して下さい';
+        document.getElementById('emailError').style.display = 'block';
+        isValid = false;
+      } else if (!pattern.test(email)) {
+        document.getElementById('emailError').textContent = 'メールアドレスの形式で入力して下さい';
+        document.getElementById('emailError').style.display = 'block';
+        isValid = false;
+      } else {
+        document.getElementById('emailError').style.display = 'none';
+      }
+
+      
 
       // バリデーションが失敗した場合、フォームの送信を防ぐ
       if (!isValid) {
