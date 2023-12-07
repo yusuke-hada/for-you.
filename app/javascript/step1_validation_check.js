5d83633441aad8d9e94ea097567e58fc0552c232
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-08 01:40:38 +0900

    ADD:password_validation

--- app/javascript/step1_validation_check.js
+++ app/javascript/step1_validation_check.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
       let isValid = true;
       let name = document.getElementById('name').value;
       let email = document.getElementById('email').value;
+      let password = document.getElementById('password').value;
+      let passwordConfirmation = document.getElementById('password_confirmation').value;
       const pattern = /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
 
       if (name.length === 0) {
@@ -29,9 +31,30 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('emailError').style.display = 'none';
       }
 
-      
+      if (password.length === 0) {
+        document.getElementById('passwordError').textContent = 'パスワードを入力して下さい';
+        document.getElementById('passwordError').style.display = 'block';
+        isValid = false;
+      } else if (password.length < 5){
+        document.getElementById('passwordError').textContent = 'パスワードは5文字以上で入力して下さい';
+        document.getElementById('passwordError').style.display = 'block';
+        isValid = false;
+      } else {
+        document.getElementById('passwordError').style.display = 'none';
+      }
 
-      // バリデーションが失敗した場合、フォームの送信を防ぐ
+      if (passwordConfirmation.length === 0) {
+        document.getElementById('passwordConfirmationError').textContent = 'パスワード(確認)を入力してください';
+        document.getElementById('passwordConfirmationError').style.display = 'block';
+        isValid = false;
+      } else if (password !== passwordConfirmation) {
+        document.getElementById('passwordConfirmationError').textContent = 'パスワードが一致しません';
+        document.getElementById('passwordConfirmationError').style.display = 'block';
+        isValid = false;
+      } else {
+        document.getElementById('passwordConfirmationError').style.display = 'none';
+      }
+
       if (!isValid) {
         event.preventDefault();
       }
