24aa387ed53c3e5516f08f0c69c80e446e201640
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-15 22:39:04 +0900

    ADD:routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
+   get 'login', to: 'user_sessions#new'  
+   post 'login', to: 'user_sessions#create'  
+   delete 'logout', to: 'user_sessions#destroy'
   root to: 'pages#top'
   get 'users/check_email', to: 'users#check_email'
   resources :users, only: %i[new create edit update] do
