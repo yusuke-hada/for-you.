0528931d891f080a0a77445713a6d3c404bc2ae3
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-15 21:40:39 +0900

    create_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   root to: 'pages#top'
   get 'users/check_email', to: 'users#check_email'
   resources :users, only: %i[new create edit update] do
