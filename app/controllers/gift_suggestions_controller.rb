23e293a24d60a3353642e3e96fa91142a1188db7
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-19 12:02:02 +0900

    ADD:api_key

---
+++ app/controllers/gift_suggestions_controller.rb
@@ -0,0 +1,18 @@
+require 'openai'
+class GiftSuggestionsController < ApplicationController
+  def index
+    if params[:question].present?
+        client = OpenAI::Client.new(access_token: Rails.application.credentials.API_KEYS[:OPENAI_API])
+        response = client.chat(
+        parameters: {
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "system", content: "対象者の性別、年齢、趣味、職業、興味に基づいて、一つのギフトアイデア（商品名のみ）を教えてください"},
+                { role: "user", content: params[:question] }
+            ]
+        })
+        @answer = response.dig("choices", 0, "message", "content")
+    end
+  end
+
+end
---
+++ test/controllers/gift_suggestions_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GiftSuggestionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
