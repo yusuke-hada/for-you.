1eb570c4da1a9c9b646963bb40c4bcaab32076c0
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-19 16:29:19 +0900

    ADD:views

--- app/models/gift_suggestion.rb
+++ app/models/gift_suggestion.rb
@@ -1,3 +1,11 @@
 class GiftSuggestion < ApplicationRecord
   belongs_to :user
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 120 }
+  validates :business, presence: true, length: { maximum: 20 }
+  validates :hobby, presence: true, length: { maximum: 20 }
+  validates :interest, presence: true, length: { maximum: 20 }
+  validates :purpose, presence: true, length: { maximum: 20 }
+  validates :relationship, presence: true, length: { maximum: 20 }
+  validates :gender, presence: true
+  enum gender: { man: 0, woman: 1, other: 2 }
 end
