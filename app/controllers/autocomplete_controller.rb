875d111ee81bad1a53b57589807a93d726fbeac7
yusuke-hada <aspringwithoutlife4929@gmail.com>
2023-12-30 00:44:29 +0900

    ADD;autocomplete_controller

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -1,17 +1,19 @@
 class AutocompleteController < ApplicationController
   def business
-    # ビジネス名に関する提案を検索して返す
+    query = params[:term]
+    suggestions = GiftSuggestion.where('business LIKE ?', "%#{query}%").distinct.limit(10)
+    render json: suggestions.map { |suggestion| { label: suggestion.business } }
   end
 
   def interest
-    # 興味・関心に関する提案を検索して返す
+  
   end
 
   def relationship
-    # 関係性に関する提案を検索して返す
+    
   end
 
   def purpose
-    # 目的に関する提案を検索して返す
+ 
   end
 end
