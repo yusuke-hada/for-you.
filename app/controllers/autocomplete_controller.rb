2324586fbfe39d453372b2fe654d7d3d6a34e0f8
yusuke-hada <aspringwithoutlife4929@gmail.com>
2024-01-01 22:51:28 +0900

    ADD:controller_interest_relatiomship_purpose

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -10,14 +10,32 @@ class AutocompleteController < ApplicationController
   end
   
   def interest
+    query = params[:q]
+  interests = GiftSuggestion.where('interest LIKE ?', "%#{query}%").distinct.pluck(:interest)
+    html = interests.map { |interest| 
+      "<li class='hover:bg-blue-400 hover:text-white' role='option'>#{interest}</li>"
+    }.join('')
   
+    render html: html.html_safe
   end
 
   def relationship
+    query = params[:q]
+    relationships = GiftSuggestion.where('relationship LIKE ?', "%#{query}%").distinct.pluck(:relationship)
+      html = relationships.map { |relationship| 
+        "<li class='hover:bg-blue-400 hover:text-white' role='option'>#{relationship}</li>"
+      }.join('')
     
+      render html: html.html_safe
   end
 
   def purpose
- 
+    query = params[:q]
+    purposes = GiftSuggestion.where('purpose LIKE ?', "%#{query}%").distinct.pluck(:purpose)
+      html = purposes.map { |purpose| 
+        "<li class='hover:bg-blue-400 hover:text-white' role='option'>#{purpose}</li>"
+      }.join('')
+    
+      render html: html.html_safe
   end
 end
