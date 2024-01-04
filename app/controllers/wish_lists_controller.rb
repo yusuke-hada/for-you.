0c04cb5c9df042e4a865a44cc109a92ab1836dd1
yusuke-hada <aspringwithoutlife4929@gmail.com>
2024-01-04 23:00:09 +0900

    ADD:destroy_action

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,4 +1,6 @@
 class WishListsController < ApplicationController
+  before_action :set_wish_list, only: [:edit,:update,:destroy]
+
   def index
     @q = WishList.ransack(params[:q])
     @wish_lists = @q.result(distinct: true).includes(:user).page(params[:page]).order("created_at desc")
@@ -11,5 +13,11 @@ class WishListsController < ApplicationController
   def destroy
     @wish_list.destroy!
     redirect_to user_wish_lists_path, alert: t('.success'), status: :see_other
+  end
+
+  private
+
+  def set_wish_list
+    @wish_list = current_user.wish_lists.find(params[:id])
   end
 end
