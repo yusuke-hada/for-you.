5a0c85ba0d7b40abab5c20b0539cb7b7a05e25b8
yusuke-hada <aspringwithoutlife4929@gmail.com>
2024-01-05 00:40:10 +0900

    ADD:search_function

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -2,4 +2,12 @@ class WishList < ApplicationRecord
   belongs_to :user
   validates :item_name, length: { maximum: 252 }, presence: true
   validates :description, length: { maximum: 252 }, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "item_name", "updated_at", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user"]
+  end
 end
