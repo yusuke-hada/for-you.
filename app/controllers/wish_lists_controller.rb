f0534dab52849daff38f28050d7d883d39a755c5
yusuke-hada <aspringwithoutlife4929@gmail.com>
2024-01-06 19:07:20 +0900

    ADD:hobby_gender_search

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -3,7 +3,13 @@ class WishListsController < ApplicationController
 
   def index
     @q = WishList.ransack(params[:q])
-    @wish_lists = @q.result(distinct: true).includes(:user).page(params[:page]).order("created_at desc")
+    @wish_lists = @q.result(distinct: true).includes(:user)
+
+    if params[:hobby].present?
+      @wish_lists = @wish_lists.joins(:user).merge(User.with_hobby(params[:hobby]))
+    end
+
+    @wish_lists = @wish_lists.page(params[:page]).order("wish_lists.created_at desc")
   end
 
   def new
