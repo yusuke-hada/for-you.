72ff5277a2621478c09cf69a2cb39e4b5076d3e2
yusuke-hada <aspringwithoutlife4929@gmail.com>
2024-01-19 17:03:17 +0900

    Update:action_mailer_for_production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "for_you__production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'foryou-9e28d89ec5bb.herokuapp.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
