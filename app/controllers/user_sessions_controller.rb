ca909a95383940836fd43f165326f2a52e13c21b
yusuke-hada <aspringwithoutlife4929@gmail.com>
2024-01-30 00:13:43 +0900

    ADD:password_securerandom

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -13,13 +13,14 @@ class UserSessionsController < ApplicationController
   end
 
   def guest_login
-    redirect_to root_path, alert: 'すでにログインしています' if current_user
+    redirect_to root_path, alert: t('.already_login') if current_user
 
     random_value = SecureRandom.hex
-    user = User.create!(name: random_value, email: "#{random_value}@example.com", password: random_value.to_s,
-                        password_confirmation: random_value.to_s, age: 20, gender: 2)
+    password_value = SecureRandom.hex
+    user = User.create!(name: random_value, email: "#{random_value}@example.com", password: password_value.to_s,
+                        password_confirmation: password_value.to_s, age: 20, gender: 2)
     auto_login(user)
-    redirect_to root_path, notice: 'ゲストとしてログインしました'
+    redirect_to root_path, notice: t('.success')
   end
 
   def destroy
