5befb40de4c8d1c2185718e40d8d27a45e9ca308
yusuke-hada <aspringwithoutlife4929@gmail.com>
2024-02-02 20:28:13 +0900

    FIX:hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,5 +106,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << 'https://www.present-for-you.jp'
+  config.hosts << 'www.present-for-you.jp'
 end
